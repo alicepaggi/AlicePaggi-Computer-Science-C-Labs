12b2354e2bf52028d2000cd7ac7bddd73a8dc578
Alice Paggi <109271167+alicepaggi@users.noreply.github.com>
2025-08-25 19:32:40 +0200

    Update first files about C language

---
+++ first_lessons/arrow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>   // Importa la libreria standard di input/output che contiene la funzione printf()
+
+int main(void) {     // Funzione principale del programma: il punto di ingresso
+    printf("  *\n"); 
+    printf(" ***\n"); 
+    printf("******\n"); 
+    printf("  *\n"); 
+    printf("  *\n"); 
+    printf("  *\n"); 
+    printf("  *\n"); 
+    printf("  *\n"); 
+    printf("  *\n");   // Stampa a schermo la stringa "Hello world!" seguita da un a capo (\n)
+    return 0;        // Indica che il programma è terminato correttamente (codice di uscita 0)
+}
---
+++ first_lessons/hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>   // Importa la libreria standard di input/output che contiene la funzione printf()
+
+int main(void) {     // Funzione principale del programma: il punto di ingresso
+    printf("Hello world!\n");   // Stampa a schermo la stringa "Hello world!" seguita da un a capo (\n)
+    return 0;        // Indica che il programma è terminato correttamente (codice di uscita 0)
+}
